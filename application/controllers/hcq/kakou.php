21f87acb23bdc8d895911fb685a550a7376bfc7d
SmellyCat <smellycat2014@foxmail.com>
2015-09-27 00:12:28 +0800

    Fix bug kkdd_id

--- application/controllers/hcq/kakou.php
+++ application/controllers/hcq/kakou.php
@@ -27,9 +27,71 @@ class Kakou extends Parsing_Controller
         header('Content-Type: application/json');
         header("HTTP/1.1 200 OK");
 
-        $this->img_ip = array('HDWJ-KKDATA1' => '10.44.249.227:81', 'HDWJ-KKDATA2' => '10.44.249.227:82');
-        $this->hpys_id = array('其他'=>1,'蓝牌'=>2, '黄牌'=>3, '白牌'=>4, '黑牌'=>5);
-        $this->fxbh_id = array('其他'=>1, '进城'=>2, '出城'=>3, '由东往西'=>4, '由南往北'=>5, '由西往东'=>6, '由北往南'=>7);
+        $this->img_ip = array(
+            'HDWJ-KKDATA1' => '10.44.249.227:81',
+            'HDWJ-KKDATA2' => '10.44.249.227:82'
+        );
+        $this->hpys_id = array(
+            '其他' => 1,
+            '蓝牌' => 2,
+            '黄牌' => 3,
+            '白牌' => 4,
+            '黑牌' => 5
+        );
+        $this->hpys_code = array(
+            '其他' => 'QT',
+            '蓝牌' => 'BU',
+            '黄牌' => 'YL',
+            '白牌' => 'WT',
+            '黑牌' => 'BK'
+        );
+        $this->fxbh_id = array(
+            '其他' => 1,
+            '进城' => 2,
+            '出城' => 3,
+            '由东往西' => 4,
+            '由南往北' => 5,
+            '由西往东' => 6,
+            '由北往南' => 7
+        );
+        $this->fxbh_code = array(
+            '其他' => 'QT',
+            '进城' => 'IN',
+            '出城' => 'OT',
+            '由东往西' => 'EW',
+            '由南往北' => 'SN',
+            '由西往东' => 'WE',
+            '由北往南' => 'NS'
+        );
+        $this->kkdd_id = array(
+            '东湖路卡口' => '441302001',
+            '交警支队卡口' => '441302002',
+            '东江大桥卡口' => '441302003',
+            '惠博路与三环路放口处卡口' => '441302004',
+            '惠沙堤二路卡口' => '441302005',
+            '陈江与镇隆交界处卡口' => '441302006',
+            '小金口卡口' => '441302007',
+            '仲恺卡口' => '441302008',
+            '水口卡口' => '441302009',
+            '马安卡口' => '441302010',
+            '陈江梧村卡口' => '441302011',
+            '惠澳大道卡口' => '441302012',
+            '惠淡路卡口' => '441302013',
+            '潼桥卡口' => '441302014',
+            '汝湖卡口' => '441302015',
+            '鳄湖路卡口' => '441302016',
+            '梅湖卡口' => '441302017',
+            '四角楼卡口' => '441302018',
+            '火车西站卡口' => '441302019',
+            '观岚大桥芦岚卡口' => '441302020',
+            '芦村卡口' => '441302021',
+            '仍图卡口' => '441302022',
+            '大岚老收费站前卡口' => '441302023',
+            '横沥卡口' => '441302024',
+            '马安龙塘桥卡口' => '441302025',
+            '马安柏田桥卡口' => '441302026',
+            '三环西路丰山卡口' => '441302027'
+        );
     }
 
 
@@ -52,12 +114,16 @@ class Kakou extends Parsing_Controller
             $items[$id]['jgsj'] = $row['PASSTIME'];
             $items[$id]['hpys'] = $row['HPYS'];
             $items[$id]['hpys_id'] = array_key_exists($row['HPYS'], $this->hpys_id) ? $this->hpys_id[$row['HPYS']] : 1;
+            $items[$id]['hpys_code'] = array_key_exists($row['HPYS'], $this->hpys_code) ? $this->hpys_code[$row['HPYS']] : 'QT';
             $items[$id]['kkdd'] = $row['WZDD'];
+            $items[$id]['kkdd_id'] = array_key_exists($row['WZDD'], $this->kkdd_id) ? $this->kkdd_id[$row['WZDD']] : null;
             $items[$id]['fxbh'] = $row['FXBH'];
             $items[$id]['fxbh_id'] = array_key_exists($row['FXBH'], $this->fxbh_id) ? $this->fxbh_id[$row['FXBH']] : 1;
+            $items[$id]['fxbh_code'] = array_key_exists($row['FXBH'], $this->fxbh_code) ? $this->fxbh_code[$row['FXBH']] : 'QT';
             $items[$id]['cdbh'] = (int)$row['CDBH'];
             $items[$id]['kkbh'] = $row['KKBH'];
-            $items[$id]['imgurl'] = "http://10.47.187.166/$row[QMTP]/" . str_replace('\\','/',$row['TJTP']);
+            $items[$id]['imgurl'] = "http://10.47.187.166/$row[QMTP]/"
+                                  . str_replace('\\', '/', $row['TJTP']);
         }
         $json = json_encode(array('total_count' => $query->num_rows(), 'items' => $items));
 
@@ -81,13 +147,17 @@ class Kakou extends Parsing_Controller
         $item['jgsj'] = $row['PASSTIME'];
         $item['hpys'] = $row['HPYS'];
         $item['hpys_id'] = array_key_exists($row['HPYS'], $this->hpys_id) ? $this->hpys_id[$row['HPYS']] : 1;
+        $item['hpys_code'] = array_key_exists($row['HPYS'], $this->hpys_code) ? $this->hpys_code[$row['HPYS']] : 'QT';
         $item['kkdd'] = $row['WZDD'];
+        $item['kkdd_id'] = array_key_exists($row['WZDD'], $this->kkdd_id) ? $this->kkdd_id[$row['WZDD']] : null;
         $item['fxbh'] = $row['FXBH'];
         $item['fxbh_id'] = array_key_exists($row['FXBH'], $this->fxbh_id) ? $this->fxbh_id[$row['FXBH']] : 1;
+        $item['fxbh_code'] = array_key_exists($row['FXBH'], $this->fxbh_code) ? $this->fxbh_code[$row['FXBH']] : 'QT';
         $item['cdbh'] = (int)$row['CDBH'];
         $item['kkbh'] = $row['KKBH'];
         #$img_ip = array('HDWJ-KKDATA1' => '192.168.1.1', 'HDWJ-KKDATA2' => '192.168.1.2');
-        $item['imgurl'] = "http://10.47.187.166/$row[QMTP]/" . str_replace('\\','/',$row['TJTP']);
+        $item['imgurl'] = "http://10.47.187.166/$row[QMTP]/"
+                        . str_replace('\\', '/', $row['TJTP']);
 
         $json = json_encode($item);
 		echo str_replace("\/", "/", $json);
--- application/controllers/hd/kakou.php
+++ application/controllers/hd/kakou.php
@@ -27,9 +27,56 @@ class Kakou extends Parsing_Controller
         header('Content-Type: application/json');
         header("HTTP/1.1 200 OK");
 
-        $this->img_ip = array('HDWJ-KKDATA1' => '10.44.249.227:81', 'HDWJ-KKDATA2' => '10.44.249.227:82');
-        $this->hpys_id = array('其他'=>1,'蓝牌'=>2, '黄牌'=>3, '白牌'=>4, '黑牌'=>5);
-        $this->fxbh_id = array('其他'=>1, '进城'=>2, '出城'=>3, '由东往西'=>4, '由南往北'=>5, '由西往东'=>6, '由北往南'=>7);
+        $this->img_ip = array(
+            'HDWJ-KKDATA1' => '10.44.249.227:81',
+            'HDWJ-KKDATA2' => '10.44.249.227:82'
+        );
+        $this->hpys_id = array(
+            '其他' => 1,
+            '蓝牌' => 2,
+            '黄牌' => 3,
+            '白牌' => 4,
+            '黑牌' => 5
+        );
+        $this->hpys_code = array(
+            '其他' => 'QT',
+            '蓝牌' => 'BU',
+            '黄牌' => 'YL',
+            '白牌' => 'WT',
+            '黑牌' => 'BK'
+        );
+        $this->fxbh_id = array(
+            '其他' => 1,
+            '进城' => 2,
+            '出城' => 3,
+            '由东往西' => 4,
+            '由南往北' => 5,
+            '由西往东' => 6,
+            '由北往南' => 7
+        );
+        $this->fxbh_code = array(
+            '其他' => 'QT',
+            '进城' => 'IN',
+            '出城' => 'OT',
+            '由东往西' => 'EW',
+            '由南往北' => 'SN',
+            '由西往东' => 'WE',
+            '由北往南' => 'NS'
+        );
+        $this->kkdd_id = array(
+            'hdk015' => '441323001',
+            'hdk12' => '441323002',
+            'hdk03' => '441323003',
+            'hdk04' => '441323004',
+            'hdk05' => '441323005',
+            'hdk08' => '441323006',
+            'hdk09' => '441323007',
+            'hdk10' => '441323008',
+            'hdk011' => '441323009',
+            'hdk13' => '441323010',
+            'hdk014' => '441323011',
+            'hdk021' => '441323012'
+        );
     }
 
 
@@ -53,9 +100,12 @@ class Kakou extends Parsing_Controller
             $items[$id]['jgsj'] = $row['PASSTIME'];
             $items[$id]['hpys'] = $row['HPYS'];
             $items[$id]['hpys_id'] = array_key_exists($row['HPYS'], $this->hpys_id) ? $this->hpys_id[$row['HPYS']] : 1;
+            $items[$id]['hpys_code'] = array_key_exists($row['HPYS'], $this->hpys_code) ? $this->hpys_code[$row['HPYS']] : 'QT';
             $items[$id]['kkdd'] = $row['WZDD'];
+            $items[$id]['kkdd_id'] = array_key_exists($row['KKBH'], $this->kkdd_id) ? $this->kkdd_id[$row['KKBH']] : null;
             $items[$id]['fxbh'] = $row['FXBH'];
             $items[$id]['fxbh_id'] = array_key_exists($row['FXBH'], $this->fxbh_id) ? $this->fxbh_id[$row['FXBH']] : 1;
+            $items[$id]['fxbh_code'] = array_key_exists($row['FXBH'], $this->fxbh_code) ? $this->fxbh_code[$row['FXBH']] : 'QT';
             $items[$id]['cdbh'] = (int)$row['CDBH'];
             $items[$id]['kkbh'] = $row['KKBH'];
             $items[$id]['imgurl'] = "http://" . @$this->img_ip[$row['TPWZ']] . "/$row[QMTP]/" . str_replace('\\','/',$row['TJTP']);
@@ -82,9 +132,12 @@ class Kakou extends Parsing_Controller
         $item['jgsj'] = $row['PASSTIME'];
         $item['hpys'] = $row['HPYS'];
         $item['hpys_id'] = array_key_exists($row['HPYS'], $this->hpys_id) ? $this->hpys_id[$row['HPYS']] : 1;
+        $item['hpys_code'] = array_key_exists($row['HPYS'], $this->hpys_code) ? $this->hpys_code[$row['HPYS']] : 'QT';
         $item['kkdd'] = $row['WZDD'];
+        $item['kkdd_id'] = array_key_exists($row['KKBH'], $this->kkdd_id) ? $this->kkdd_id[$row['KKBH']] : null;
         $item['fxbh'] = $row['FXBH'];
         $item['fxbh_id'] = array_key_exists($row['FXBH'], $this->fxbh_id) ? $this->fxbh_id[$row['FXBH']] : 1;
+        $item['fxbh_code'] = array_key_exists($row['FXBH'], $this->fxbh_code) ? $this->fxbh_code[$row['FXBH']] : 'QT';
         $item['cdbh'] = (int)$row['CDBH'];
         $item['kkbh'] = $row['KKBH'];
         #$img_ip = array('HDWJ-KKDATA1' => '192.168.1.1', 'HDWJ-KKDATA2' => '192.168.1.2');
